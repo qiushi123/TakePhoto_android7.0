de9dfad94c373b44c64287cae6928d2d54db2a2f
qcl <2501902696@qq.com>
2017-05-26 18:32:24 +0800

    安卓7.0拍照权限适配

---
+++ app/src/main/java/com/takephoto_android70/MainActivity.java
@@ -0,0 +1,161 @@
+package com.takephoto_android70;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+
+import kr.co.namee.permissiongen.PermissionFail;
+import kr.co.namee.permissiongen.PermissionGen;
+import kr.co.namee.permissiongen.PermissionSuccess;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTakePhoto;
+    private Button mBtnSelectPhoto;
+    private TextView mTvPath;
+    private TextView mTvUri;
+    private LQRPhotoSelectUtils mLqrPhotoSelectUtils;
+    private ImageView mIvPic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnTakePhoto = (Button) findViewById(R.id.btnTakePhoto);
+        mBtnSelectPhoto = (Button) findViewById(R.id.btnSelectPhoto);
+        mTvPath = (TextView) findViewById(R.id.tvPath);
+        mTvUri = (TextView) findViewById(R.id.tvUri);
+        mIvPic = (ImageView) findViewById(R.id.ivPic);
+
+        init();
+        initListener();
+
+
+    }
+
+    private void init() {
+        // 1、创建LQRPhotoSelectUtils（一个Activity对应一个LQRPhotoSelectUtils）
+        mLqrPhotoSelectUtils = new LQRPhotoSelectUtils(this, new LQRPhotoSelectUtils.PhotoSelectListener() {
+            @Override
+            public void onFinish(File outputFile, Uri outputUri) {
+                // 4、当拍照或从图库选取图片成功后回调
+                mTvPath.setText(outputFile.getAbsolutePath());
+                mTvUri.setText(outputUri.toString());
+                Glide.with(MainActivity.this).load(outputUri).into(mIvPic);
+            }
+        }, false);//true裁剪，false不裁剪
+
+        //        mLqrPhotoSelectUtils.setAuthorities("com.lqr.lqrnativepicselect.fileprovider");
+        //        mLqrPhotoSelectUtils.setImgPath(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + String.valueOf(System.currentTimeMillis()) + ".jpg");
+    }
+
+    private void initListener() {
+        mBtnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 3、调用拍照方法
+                PermissionGen.with(MainActivity.this)
+                        .addRequestCode(LQRPhotoSelectUtils.REQ_TAKE_PHOTO)
+                        .permissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.CAMERA
+                        ).request();
+            }
+        });
+
+        mBtnSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 3、调用从图库选取图片方法
+                PermissionGen.needPermission(MainActivity.this,
+                        LQRPhotoSelectUtils.REQ_SELECT_PHOTO,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                );
+            }
+        });
+    }
+
+    @PermissionSuccess(requestCode = LQRPhotoSelectUtils.REQ_TAKE_PHOTO)
+    private void takePhoto() {
+        mLqrPhotoSelectUtils.takePhoto();
+    }
+
+    @PermissionSuccess(requestCode = LQRPhotoSelectUtils.REQ_SELECT_PHOTO)
+    private void selectPhoto() {
+        mLqrPhotoSelectUtils.selectPhoto();
+    }
+
+    @PermissionFail(requestCode = LQRPhotoSelectUtils.REQ_TAKE_PHOTO)
+    private void showTip1() {
+        //        Toast.makeText(getApplicationContext(), "不给我权限是吧，那就别玩了", Toast.LENGTH_SHORT).show();
+        showDialog();
+    }
+
+    @PermissionFail(requestCode = LQRPhotoSelectUtils.REQ_SELECT_PHOTO)
+    private void showTip2() {
+        //        Toast.makeText(getApplicationContext(), "不给我权限是吧，那就别玩了", Toast.LENGTH_SHORT).show();
+        showDialog();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        PermissionGen.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 2、在Activity中的onActivityResult()方法里与LQRPhotoSelectUtils关联
+        mLqrPhotoSelectUtils.attachToActivityForResult(requestCode, resultCode, data);
+    }
+
+    public void showDialog() {
+        //创建对话框创建器
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //设置对话框显示小图标
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        //设置标题
+        builder.setTitle("权限申请");
+        //设置正文
+        builder.setMessage("在设置-应用-虎嗅-权限 中开启相机、存储权限，才能正常使用拍照或图片选择功能");
+
+        //添加确定按钮点击事件
+        builder.setPositiveButton("去设置", new DialogInterface.OnClickListener() {//点击完确定后，触发这个事件
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //这里用来跳到手机设置页，方便用户开启权限
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + MainActivity.this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        //添加取消按钮点击事件
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        //使用构建器创建出对话框对象
+        AlertDialog dialog = builder.create();
+        dialog.show();//显示对话框
+    }
+}
